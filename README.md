677a01163f9454499dd868a89ebba94e65653f8e
Maksim Svetlakov <maksuttt@gmai.com>
2024-02-07 18:58:25 -0500

    added_pwa

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 минус
 
 logic not to duplicate commands: if landed, command to land should be questioned
+
+wakewords:
+caramba
+mario
+don pedro
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 
@@ -12,4 +13,19 @@ root.render(
   </React.StrictMode>
 );
 
+// if ('serviceWorker' in navigator) {
+//   // Register a service worker hosted at the root of the
+//   // site using the default scope.
+//   navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/service-worker.js`).then(
+//     registration => {
+//       console.log('Service worker registration succeeded:', registration);
+//     },
+//     /*catch*/ error => {
+//       console.error(`Service worker registration failed: ${error}`);
+//     }
+//   );
+// } else {
+//   console.error('Service workers are not supported.');
+// }
 
+serviceWorkerRegistration.register();
